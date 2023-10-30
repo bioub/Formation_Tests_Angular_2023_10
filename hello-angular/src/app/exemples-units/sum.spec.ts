8c99e6e52836130ea00be2b37eda4d4801a538d6
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-10-30 13:49:57 +0100

    Jour 0.5

---
+++ hello-angular/src/app/exemples-units/sum.spec.ts
@@ -0,0 +1,10 @@
+import { sum } from "./sum";
+
+describe('sum function', () => {
+
+  it('should add positive numbers', () => {
+    expect(sum(1, 2)).toBe(3);
+    expect().nothing();
+  });
+
+});
