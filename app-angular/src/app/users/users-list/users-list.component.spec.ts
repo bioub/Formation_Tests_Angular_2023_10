dafed17f784794256bf6d0f0ac1a693644ef725d
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-10-31 16:11:20 +0100

    Correction spyOn

--- app-angular/src/app/users/users-list/users-list.component.spec.ts
+++ app-angular/src/app/users/users-list/users-list.component.spec.ts
@@ -3,38 +3,39 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { UsersListComponent } from './users-list.component';
+import { UserService } from '../user.service';
+import { of } from 'rxjs';
 
 describe('UsersListComponent', () => {
   let component: UsersListComponent;
   let fixture: ComponentFixture<UsersListComponent>;
-  let originalConsoleError!: any;
-
-  beforeAll(() => {
-    originalConsoleError = console.error;
-    console.error = function (message?: any, ...optionalParams: any[]): void {
-      const params = optionalParams ? `\nParams: ${optionalParams}` : '';
-      fail(`Test contained console error:\n${message}${params}`);
-    };
-  });
-
-  afterAll(() => {
-    console.error = originalConsoleError;
-  });
+  let nativeElement: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UsersListComponent],
       imports: [RouterTestingModule.withRoutes([]), HttpClientTestingModule],
+      errorOnUnknownElements: true,
+      errorOnUnknownProperties: true,
     }).compileComponents();
   });
 
   beforeEach(() => {
+    const userService = TestBed.inject(UserService);
+    spyOn(userService, 'getList$').and.returnValue(of([{id: 1, name: "Toto"}, { id: 2, name: 'Titi' }]))
+
     fixture = TestBed.createComponent(UsersListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    nativeElement = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display data from userService (spyOn)', () => {
+    expect(nativeElement.textContent).toContain('Toto');
+    expect(nativeElement.textContent).toContain('Titi');
   });
 });
